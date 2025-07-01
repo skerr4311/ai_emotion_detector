3e0d768631b589e4c88c421186ca0fa0e0e5c7b1
skerr4311 <stevenkerr4311@gmail.com>
2025-07-01 16:00:30 -0400

    start the emotion_detector

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,1 +1,20 @@
-# Getting ready
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {
+        "Content-Type": "application/json",
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    
+    response = requests.post(url, headers=headers, json=input_json)
+    
+    # Raise an error if request fails
+    response.raise_for_status()
+
+    # Print the response for debugging
+    print(response.json())
+    
+    # Return the 'text' attribute of the response json
+    return response.json()['text']
